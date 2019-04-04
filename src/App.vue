be415385a461272aa5ab1fb266a434fcf7527c21
McK <33714767+McKensy@users.noreply.github.com>
2019-04-04 16:40:05 +0200

    idk

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyList from './components/MyList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyList
   }
 }
 </script>
---
+++ src/components/MyList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1 
+    v-if="showing"
+    v-bind:title="msg">
+    something else
+    </h1>
+    <!--<p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>-->
+    <input type="text" v-model="msg">
+    <button v-on:click="addNewFood">Add grapes</button>
+    <ul>
+      <li v-for="food in foods" v-bind:key="food.foodId">
+        {{ food.foodName }}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyList',
+  data() {
+    return {
+      msg: 'give me food',
+      showing: true,
+      foods: [
+        {foodId: 'p', foodName: 'pumpkin'},
+        {foodId: 'm', foodName: 'melon'},
+        {foodId: 'i', foodName: 'pie'}
+      ]
+    }
+  },
+  methods: {
+    addNewFood: function() {
+      this.foods.push({foodId: 'g', foodName: this.msg})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
