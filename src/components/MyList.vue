1d385c2a4245ebe3efe4425f1b9a194a28785df2
McK <33714767+McKensy@users.noreply.github.com>
2019-04-24 11:55:12 +0200

    yeet

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -3,20 +3,17 @@
     <h1 
     v-if="showing"
     v-bind:title="msg">
-    something else
+    give me the {{item.foodName}}
+    <span v-if="err">, {{fail}}</span>
     </h1>
+    
     <!--<p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>-->
-    <input type="text" v-model="msg">
-    <button v-on:click="addNewFood">Add grapes</button>
-    <ul>
-      <li v-for="food in foods" v-bind:key="food.foodId">
-        {{ food.foodName }}
-      </li>
-    </ul>
+    <input type="text" v-model="msg" @keyup.enter="getItem()">
+    <button v-on:click="getItem">Add grapes</button>
 
   </div>
 </template>
@@ -26,25 +23,52 @@ export default {
   name: 'MyList',
   data() {
     return {
-      msg: 'give me food',
+      msg: '',
+      fail: 'NO! I dont want that!',
+      err: false,
       showing: true,
       foods: [
-        {foodId: 'p', foodName: 'pumpkin'},
-        {foodId: 'm', foodName: 'melon'},
-        {foodId: 'i', foodName: 'pie'}
-      ]
+        {foodId: 1, foodName: 'pumpkin'},
+        {foodId: 2, foodName: 'melon'},
+        {foodId: 3, foodName: 'pie'},
+        {foodId: 4, foodName: 'cheese'},
+        {foodId: 5, foodName: '*#*+11?~~'},
+        {foodId: 7, foodName: 'bread'},
+        {foodId: 8, foodName: 'water'},
+        {foodId: 9, foodName: 'apple'},
+        {foodId: 10, foodName: 'samsung'},
+        {foodId: 11, foodName: 'pain'},
+        {foodId: 12, foodName: 'Multivitaminsaft'},
+        {foodId: 13, foodName: 'AK-47'}
+      ],
+      item: []
     }
   },
   methods: {
-    addNewFood: function() {
-      this.foods.push({foodId: 'g', foodName: this.msg})
+    getItem(){
+      if(this.msg == this.item.foodName){
+        let rand = Math.floor(Math.random() * this.foods.length)
+        this.item = this.foods[rand]
+        this.err = false
+      }else{
+        this.err = true;
+      }
+
+      this.msg = ''
     }
+  },
+  mounted(){
+    let rand = Math.floor(Math.random() * this.foods.length)
+    this.item = this.foods[rand]
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1{
+  background-color: whitesmoke;
+}
 h3 {
   margin: 40px 0 0;
 }
